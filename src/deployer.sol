5b8e7a437a00bb5578250275bf3643353e742d34
Ankit Basu <ankitbasu14@gmail.com>
2025-06-17 18:59:08 +0000

    wrote the poolManager contract

--- src/deployer.sol
+++ src/deployer.sol
@@ -6,36 +6,69 @@ import {Factory} from "./factory.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Deployer is Ownable {
+    // errors
     error Deployer__Unauthorized();
 
+    // events
     event Deployer__PoolDeployed(
+        uint256 chainId,
         address indexed poolAddress,
         address indexed token,
         string cidHash
     );
 
-    uint256 public poolId;
+    // state variables
+    uint256 public s_poolId;
+    address private immutable i_ccipSender;
 
     Factory public factory;
 
-    constructor() Ownable(msg.sender) {
-        poolId = 0;
+    // constructor
+    constructor(address _ccipSender) Ownable(msg.sender) {
+        s_poolId = 0;
+        i_ccipSender = _ccipSender;
     }
 
+    // functions
+
+    /**
+     * @dev Deploys a new PoolManager contract. Used when a new pool is to be created.
+     * @param _token The address of the token for the pool.
+     * @param _cidHash The CID hash of the pool's metadata.
+     * @return The address of the newly deployed PoolManager contract.
+     * NOTE: 1. The function is called on the the respective contract deployed on all the chains.
+     *       2. Only callable by the factory contract.
+     */
     function deployNewPool(
         address _token,
-        string memory _cidHash
+        string memory _cidHash,
+        address _fm
     ) external returns (address) {
         if (msg.sender != address(factory)) {
             revert Deployer__Unauthorized();
         }
-        PoolManager poolManager = new PoolManager(_token, _cidHash, poolId);
-        poolId++;
+        PoolManager poolManager = new PoolManager(
+            _token,
+            _cidHash,
+            s_poolId,
+            i_ccipSender,
+            _fm
+        );
+        s_poolId++;
 
-        emit Deployer__PoolDeployed(address(poolManager), _token, _cidHash);
+        emit Deployer__PoolDeployed(
+            block.chainid,
+            address(poolManager),
+            _token,
+            _cidHash
+        );
         return address(poolManager);
     }
 
+    /**
+     * @dev Sets the factory contract address. Only callable by the owner.
+     * @param _factory The address of the factory contract.
+     */
     function setFactory(address _factory) external onlyOwner {
         factory = Factory(_factory);
     }
--- src/poolManager.sol
+++ src/poolManager.sol
@@ -1,6 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {CCIPSender} from "./ccipSender.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract PoolManager {
-    constructor(address _token, string memory _cidHash, uint256 _poolId) {}
+    using SafeERC20 for IERC20;
+
+    error PoolManager__Not_Fund_Manager();
+    error PoolManager__Insufficient_Amount();
+
+    event PoolManager__LiquidityAdded(
+        uint256 indexed poolId,
+        address depositor,
+        uint256 amount
+    );
+
+    enum Tier {
+        T1,
+        T2,
+        T3
+    }
+
+    address public immutable i_token;
+    string public s_cidHash;
+    uint256 public immutable i_poolId;
+    CCIPSender public immutable i_ccipSender;
+
+    mapping(address => bool) public s_isFM;
+    mapping(address => Tier) public s_tiers;
+    mapping(address => uint256) public s_fmWithdrawn; // total amount withdrawn by each fund manager
+    mapping(Tier => uint256) public s_tierLimits; // percentage of the total liquidity that a fund manager of a specific tier can withdraw at a time
+
+    modifier onlyFM() {
+        if (!s_isFM[msg.sender]) {
+            revert PoolManager__Not_Fund_Manager();
+        }
+        _;
+    }
+
+    constructor(
+        address _token,
+        string memory _cidHash,
+        uint256 _poolId,
+        address _ccipSender,
+        address _fm
+    ) {
+        i_token = _token;
+        s_cidHash = _cidHash;
+        i_poolId = _poolId;
+        i_ccipSender = CCIPSender(_ccipSender);
+        s_isFM[_fm] = true;
+        s_tiers[_fm] = Tier.T1;
+
+        s_tierLimits[Tier.T1] = 10;
+        s_tierLimits[Tier.T2] = 15;
+        s_tierLimits[Tier.T3] = 20;
+    }
+
+    function addLiquidity(uint256 _amount) public {
+        if (_amount == 0) {
+            revert PoolManager__Insufficient_Amount();
+        }
+        IERC20(i_token).safeTransferFrom(msg.sender, address(this), _amount);
+        IERC20(i_token).approve(address(i_ccipSender), _amount);
+        if (block.chainid == 11155111) {
+            // logic to be added
+        } else {
+            i_ccipSender.sendTokens(
+                11155111, // destination chain ID for Sepolia
+                i_token,
+                address(0), // receiver address(pool) to be added
+                _amount
+            );
+        }
+
+        //logic to mint lp tokens to the depositor to be added
+    }
 }
